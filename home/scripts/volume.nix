1d6c1fdb0523c2772bbef8c7ebdd9b7cc042a370
installer <yasirfadhil46@gmail.com>
2025-06-05 22:55:10 +0700

    as

---
+++ home/scripts/volume.nix
@@ -0,0 +1,50 @@
+{ pkgs, ... }:
+
+pkgs.writeShellScriptBin "volume-control" ''
+  SINK=$(${pkgs.pulseaudio}/bin/pactl info | grep 'Default Sink' | awk '{print $3}')
+
+  get_volume() {
+    ${pkgs.pulseaudio}/bin/pactl get-sink-volume "$SINK" | grep -o '[0-9]\+%' | head -1
+  }
+
+  get_mute() {
+    ${pkgs.pulseaudio}/bin/pactl get-sink-mute "$SINK" | awk '{print $2}'
+  }
+
+  play_feedback_sound() {
+    ${pkgs.libcanberra}/bin/canberra-gtk-play -i audio-volume-change -d "volume-feedback" &>/dev/null &
+  }
+
+  case "$1" in
+    up)
+      ${pkgs.pulseaudio}/bin/pactl set-sink-volume "$SINK" +5%
+      play_feedback_sound
+      echo "Volume: $(get_volume)"
+      ;;
+    down)
+      ${pkgs.pulseaudio}/bin/pactl set-sink-volume "$SINK" -5%
+      play_feedback_sound
+      echo "Volume: $(get_volume)"
+      ;;
+    mute)
+      ${pkgs.pulseaudio}/bin/pactl set-sink-mute "$SINK" toggle
+      if [ "$(get_mute)" = "yes" ]; then
+        ${pkgs.libcanberra}/bin/canberra-gtk-play -i audio-volume-muted -d "volume-feedback" &>/dev/null &
+        echo "Muted"
+      else
+        play_feedback_sound
+        echo "Unmuted - Volume: $(get_volume)"
+      fi
+      ;;
+    status)
+      if [ "$(get_mute)" = "yes" ]; then
+        echo "Muted"
+      else
+        echo "Volume: $(get_volume)"
+      fi
+      ;;
+    *)
+      echo "Usage: $0 {up|down|mute|status}"
+      ;;
+  esac
+''
