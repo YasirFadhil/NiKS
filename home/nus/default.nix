e3d952bc1fa26e0eb0ecd1facddce2f6f5ef737e
installer <yasirfadhil46@gmail.com>
2025-06-06 16:59:40 +0700

    Create default.nix

---
+++ home/nus/default.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+{
+  programs.nushell = {
+    enable = true;
+    configFile.text = ''
+      # Startup commands
+      fastfetch -c ~/.config/fastfetch/config-compact.jsonc
+      #pokemon-colorscripts --no-title -s -r
+
+      $env.config.show_banner = false
+
+      source ~/.zoxide.nu
+      source ~/.cache/carapace/init.nu
+
+      # Custom git commands
+      def git-up [] {
+        let msg = (input "Commit message: ")
+        git add .
+        git commit -m $msg
+        git push -u origin main
+      }
+
+      def git-we3pro [] {
+        let msg = (input "Commit message: ")
+        git add .
+        git commit -m $msg
+        git push -u update main
+      }
+
+      def git-we [] {
+        let msg = (input "Commit message: ")
+        git add .
+        git commit -m $msg
+        git push -u ori main
+      }
+
+      # Alias shortcut
+      alias l = clear
+      alias g = git
+      alias cd = z
+      alias cok = fuck
+      alias zed = zeditor
+
+      # Zoxide and Carapace initialization (migrated from env.nu)
+      zoxide init nushell | save -f ~/.zoxide.nu
+      $env.CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense'
+      mkdir ~/.cache/carapace
+      carapace _carapace nushell | save --force ~/.cache/carapace/init.nu
+
+      # If you want to modify PATH or other env, use envFile.text or home.sessionPath.
+      # Example: $env.PATH | append "/home/kairu/.nix-profile/bin"
+    '';
+    # Optional: If you want to set environment variables or PATH
+    # envFile.text = ''
+    #   $env.PATH | append "/home/kairu/.nix-profile/bin"
+    # '';
+  };
+  programs.starship.enable = true;
+}
