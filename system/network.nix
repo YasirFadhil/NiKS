f81c569875695bbc1f03fdafd0897e49000db075
installer <yasirfadhil46@gmail.com>
2025-06-17 19:44:18 +0700

    nih

--- system/network.nix
+++ system/network.nix
@@ -2,23 +2,30 @@
 
 {
   networking.networkmanager.enable = true;
-  networking.firewall.enable = true;
   services.dbus.enable = true;
   security.polkit.enable = true;
   programs.kdeconnect.enable = true;
-
+  # Firewall
+     networking.firewall = {
+       enable = true;
+       allowedTCPPorts = [80 443 1935 3000 8080 8081 9000];
+       allowedUDPPorts = [3478 3479];
+       allowedTCPPortRanges = [
+         { from = 1714; to = 1764; }
+       ];
+       allowedUDPPortRanges = [
+         { from = 1714; to = 1764; }
+       ];
+     };
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
 
-  networking.firewall.allowedTCPPorts = [ 80 443 1935 8080 8081 ];
-  networking.firewall.allowedUDPPorts = [ 3478 3479 ];
-
   #KDE connect
-  networking.firewall.allowedTCPPortRanges = [
-    { from = 1714; to = 1764; }
-  ];
-  networking.firewall.allowedUDPPortRanges = [
-    { from = 1714; to = 1764; }
-  ];
+  # networking.firewall.allowedTCPPortRanges = [
+  #   { from = 1714; to = 1764; }
+  # ];
+  # networking.firewall.allowedUDPPortRanges = [
+  #   { from = 1714; to = 1764; }
+  # ];
 
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
