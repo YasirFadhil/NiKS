8d7d574a6bf8f05e9ce2ebc53c7ac7bd792b1569
YasirFadhil <yasirfadhil46@gmail.com>
2025-07-09 15:22:58 +0700

    idk what is this

--- home/nus/default.nix
+++ home/nus/default.nix
@@ -1,6 +1,13 @@
-{
+{ pkgs, ... }:{
+
   imports = [
-    ./nus.nix
     ./starship.nix
   ];
+
+  programs.nushell = {
+    enable = true;
+      configFile.source = ./config.nu;
+      envFile.source = ./env.nu;
+  };
+  programs.starship.enable = true;
 }
