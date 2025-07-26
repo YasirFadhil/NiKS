7585180bfd0255953317b06cdb003cccf8b7631e
YasirFadhil <yasirfadhil46@gmail.com>
2025-07-26 09:45:14 +0700

    change theme

--- home/spicetify/default.nix
+++ home/spicetify/default.nix
@@ -1,15 +1,18 @@
 # Spicetify is a spotify client customizer
-{ pkgs, config, lib, inputs, ... }:
-let
+{
+  pkgs,
+  config,
+  lib,
+  inputs,
+  ...
+}: let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in {
-  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
-
+  imports = [inputs.spicetify-nix.homeManagerModules.default];
 
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
+    theme = lib.mkForce spicePkgs.themes.text;
 
     enabledExtensions = with spicePkgs.extensions; [
       playlistIcons
