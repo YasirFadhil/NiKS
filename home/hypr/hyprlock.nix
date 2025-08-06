b55a379b1efe0bec92a696829a3a141f2567141d
YasirFadhil <yasirfadhil46@gmail.com>
2025-08-06 13:38:30 +0700

    monte

--- home/hypr/hyprlock.nix
+++ home/hypr/hyprlock.nix
@@ -17,7 +17,7 @@ in {
 
       background = lib.mkForce {
         monitor = "";
-        path = "/etc/nixos/themes/pict/tahu26.jpg";
+        path = "/etc/nixos/themes/pict/mont.jpg";
         # You can add blur, vibrancy, contrast if you want.
       };
 
