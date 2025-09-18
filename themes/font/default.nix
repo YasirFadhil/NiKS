5758524ac067a2d60063831b5d4eee4c7f5cebae
YasirFadhil <yasirfadhil46@gmail.com>
2025-09-18 15:09:49 +0700

    font

--- themes/font/default.nix
+++ themes/font/default.nix
@@ -1,7 +1,9 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, ... }: {
   fonts.packages = with pkgs; [
-    inputs.apple-fonts.packages.${pkgs.system}.sf-pro
-    inputs.apple-fonts.packages.${pkgs.system}.sf-mono
-    # inputs.apple-fonts.packages.${pkgs.system}.new-york
+    inter               # Similar to SF Pro
+    jetbrains-mono      # Great monospace font
+    noto-fonts          # Comprehensive font family
+    noto-fonts-cjk-sans # CJK support
+    ubuntu_font_family  # Clean, modern font
   ];
 }
