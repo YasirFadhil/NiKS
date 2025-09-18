320dc41fe207d80fad866f698c90defea931e7f6
YasirFadhil <yasirfadhil46@gmail.com>
2025-09-18 15:09:43 +0700

    ventoy

--- system/system-env.nix
+++ system/system-env.nix
@@ -2,38 +2,38 @@
 
 {
   environment.systemPackages = with pkgs; [
-	wget
+    wget
   	udisks
   	gnome-disk-utility
   	adwaita-icon-theme
   	gsettings-desktop-schemas
   	librsvg
- 	gvfs
-  	dconf-editor
-  	dbus
   	gvfs
-  	udisks
+    dconf-editor
+    dbus
+    gvfs
+    udisks
   	zed-editor
-	fish
-	zsh
-	nushell
-	oh-my-zsh
-	zsh-syntax-highlighting
-	vim
-	neovim
-  	spice-gtk
-	efibootmgr
-	pamixer
-	brightnessctl
-  	mediawriter
-  	ventoy-full
-  	python
-  	# vmware-wokstation
+   fish
+   zsh
+   nushell
+   oh-my-zsh
+   zsh-syntax-highlighting
+   vim
+   neovim
+   spice-gtk
+   efibootmgr
+   pamixer
+   brightnessctl
+	 mediawriter
+   ventoy-full
+   python
+   # vmware-wokstation
   ];
 
 
   nixpkgs.config.permittedInsecurePackages = [
-  "ventoy-1.1.05"
+  "ventoy-1.1.07"
   "python-2.7.18.8"
 
   ];
