0ec63739e180c37dca18818d14afc0fe2cab5949
YasirFadhil <yasirfadhil46@gmail.com>
2025-11-16 17:43:04 +0700

    fixing fonts and theming

--- home/themes/default.nix
+++ home/themes/default.nix
@@ -4,51 +4,22 @@
   imports = [
     ./gtk
     ./qt
-    ./font/fontconfig.nix
+    ./font
   ];
 
   # Common theme-related packages
   home.packages = with pkgs; [
     # Theme utilities
     dconf-editor
-  ] ++ (import ./font { inherit pkgs; });
+  ];
 
   # Common theme environment variables
   home.sessionVariables = {
-    # Cursor theme (shared between GTK and Qt)
+    # Cursor theme (Bibata Modern Ice)
     XCURSOR_THEME = "Bibata-Modern-Ice";
     XCURSOR_SIZE = "20";
 
     # Backend preferences
     GDK_BACKEND = "wayland,x11,*";
-  };
-
-  # XDG settings for consistent theming
-  xdg.configFile."fontconfig/fonts.conf" = {
-    text = ''
-      <?xml version="1.0"?>
-      <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-      <fontconfig>
-        <alias>
-          <family>monospace</family>
-          <prefer>
-            <family>JetBrainsMono Nerd Font</family>
-            <family>Noto Sans Mono</family>
-          </prefer>
-        </alias>
-        <alias>
-          <family>sans-serif</family>
-          <prefer>
-            <family>Noto Sans</family>
-          </prefer>
-        </alias>
-        <alias>
-          <family>serif</family>
-          <prefer>
-            <family>Noto Serif</family>
-          </prefer>
-        </alias>
-      </fontconfig>
-    '';
   };
 }
